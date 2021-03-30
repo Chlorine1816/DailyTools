365cc3b29f01fcaf96900746c3cb25f1ee0192ca
Chlorine <Chlorine1816@189.cn>
2021-03-30 13:58:37 +0800

    update

--- code/Fund.py
+++ code/Fund.py
@@ -166,6 +166,7 @@ def working(code):
         writing('基金速查 估值：',jz_data[-1],gszf1)
         writing('天天基金 估值：',jz_data[-1],gszf2)
         writing('均值修正 估值：',jz_data[-1],(gszf1+gszf2)/2)
+        name=name.strip('(')[0]
         sio_digest.write(f'{name} 💕\n')
     elif (today_lj > num_mean) and (today_lj < q3):
         sio_content.write(f'<div>💗</div>')
@@ -174,6 +175,7 @@ def working(code):
         writing('基金速查 估值：',jz_data[-1],gszf1)
         writing('天天基金 估值：',jz_data[-1],gszf2)
         writing('均值修正 估值：',jz_data[-1],(gszf1+gszf2)/2)
+        name=name.strip('(')[0]
         sio_digest.write(f'{name} 💗\n')
     elif (today_lj > q4):
         sio_content.write(f'<div>🚀</div>')
@@ -182,6 +184,7 @@ def working(code):
         writing('基金速查 估值：',jz_data[-1],gszf1)
         writing('天天基金 估值：',jz_data[-1],gszf2)
         writing('均值修正 估值：',jz_data[-1],(gszf1+gszf2)/2)
+        name=name.strip('(')[0]
         sio_digest.write(f'{name} 🚀\n')
     else:
         sio_content.write(f'<div>💚</div>')
