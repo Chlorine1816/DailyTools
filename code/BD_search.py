de771156263d689bdbbcde7146a5d2e6a35ac5dd
Chlorine <Chlorine1816@189.cn>
2021-07-01 17:17:57 +0800

    添加百度热搜

---
+++ code/BD_search.py
@@ -0,0 +1,84 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+from io import StringIO
+import json
+import os
+
+headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.74'}
+
+corpid=os.environ['CORPID']  #公司id
+agentid=os.environ['AGENTID']  #机器人id
+corpsecret=os.environ['CORPSECRET']  #机器人secret
+media_id=os.environ['MEDIA'] #图片id
+touser=f'@all'  #接收id
+
+#图文消息的描述，不超过512个字节
+sio_digest=StringIO('')
+sio_digest.write(time.strftime(f'%Y-%m-%d UTC(%H:%M)', time.localtime())+'\n')
+#图文消息的内容，支持html标签，不超过666 K个字节
+sio_content=StringIO('') 
+
+def get_token():
+    payload_access_token = {'corpid': corpid, 'corpsecret': corpsecret}
+    token_url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
+    r = requests.get(url=token_url, params=payload_access_token,headers=headers)
+    dict_result = (r.json())
+    return dict_result['access_token']
+
+#发送图文信息
+def send_mpnews(title,content,digest):
+    time.sleep(0.2)
+    access_token=get_token()
+    url = f"https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}"
+    data = {
+        "touser":touser, #接收消息人员id
+        "agentid": agentid, #机器人id
+        'msgtype':'mpnews',
+        'mpnews': {
+            'articles':[
+                {
+                    "title": title, #必须
+                    "thumb_media_id": media_id, #必须
+                    "author": "Chlorine", #非必须 作者
+                    "content": content, #必须 不超过666 K个字节
+                    "digest": digest #非必须 不超过512个字节
+                }
+            ]
+            },
+        "safe": 0 #是否加密
+        }
+    data = json.dumps(data, ensure_ascii=False)
+    requests.post(url=url, data=data.encode("utf-8").decode("latin1"))
+
+def get_BDrealtime():
+    url='https://top.baidu.com/board'
+    headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.74'}
+    data={'tab':'realtime'}
+    #图文图文消息的标题
+    title=f'百度热搜'
+    try:
+        req=requests.get(url=url,params=data,headers=headers)
+        req.encoding='utf-8'
+        if req.status_code==200:
+            html=req.text
+    except:
+        html=''
+    bf=BeautifulSoup(html,'lxml')
+    bf=bf.find_all('div',class_='content_1YWBm')
+    for i in range(22):
+        cache=bf[i].find_all('a')[0]
+        words=str(cache.text).strip().split(' ')[0]
+        link=cache.get('href')
+        if i < 10:
+            sio_digest.write(f'🔥{words}\n')
+        sio_content.write(f'<div>[{i+1}]<a href=\"{link}\">{words}</a></div>')
+    send_mpnews(title,sio_content.getvalue(),sio_digest.getvalue())
+
+def main(arg1,arg2):
+    get_BDrealtime()
+
+if __name__=='__main__':
+    arg1=0
+    arg2=0
+    main(arg1,arg2)
