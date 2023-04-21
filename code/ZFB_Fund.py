e67008723b3d4bd5a5361545228b0140829c7335
Chlorine1816 <Chlorine1816@139.com>
2023-04-21 15:20:04 +0800

    更新

--- code/ZFB_Fund.py
+++ code/ZFB_Fund.py
@@ -136,7 +136,14 @@ def get_color(ljjz_data):
     mean5=round(mean(ljjz_data[-5:]),4) #前5天净值均值
     mean10=round(mean(ljjz_data[-10:]),4)#前10天净值均值
     mean20=round(mean(ljjz_data[-20:]),4)#前20天净值均值
-    return(min(mean5,mean10,mean20),max(mean5,mean10,mean20))
+
+    if (mean5 <= mean10 <= mean20):
+        return 'green'
+    elif (mean5 >= mean10 >= mean20):
+        return 'red'
+    else:
+        return 'black'
+    #return(min(mean5,mean10,mean20),max(mean5,mean10,mean20))
 
 def working(code):
     data=get_his(code) #获取历史净值
@@ -155,22 +162,22 @@ def working(code):
         lj_data=np.append(lj_data,today_lj) #前1季度累计净值+当日估值
         color='red' if gszf > 0 else 'green'
 
-    num_min20,num_max20=get_color(lj_data) #求近20天均值极值点
+    today_color=get_color(lj_data) #求近20天均值极值点
 
     state,tip=pd_jz(lj_data,today_lj)
     sio_content1=''
     sio_content2=''
     sio_content3=''
-    if (tip > 85)and(today_lj >= num_max20):
+    if (tip > 80)and(today_color=='black'):
         sio_content2=f'<p>{state} <font color="red"><small>{tip}%</small></font></p>'
         sio_content2+=f'<p><font color="red"><strong>{name}</strong></font><font color="{color}"><small> {gszf}%</small></font></p>'
         sio_content2+=f'<p>卖出<font color="red"> {round(max(tip-85,5)*0.88/(dwjz+zf),1)} </font>份</p>'
-    elif (tip < 20)and(today_lj <= num_min20):
+    elif (tip < 20)and(today_color=='black'):
         sio_content1=f'<p>{state} <font color="green"><small>{tip}%</small></font></p>'
         sio_content1+=f'<p><font color="green"><strong>{name}</strong></font><font color="{color}"><small> {gszf}%</small></font></p>'
         sio_content1+=f'<p>买入 <font color="green">{round(max(26-tip,10),1)}</font> 元</p>'
     else:
-        sio_content3=f'<p>{state} <font color="black"><small>{tip}%</small></font></p>'
+        sio_content3=f'<p>{state} <font color={today_color}><small>{tip}%</small></font></p>'
         sio_content3+=f'<p>{name}<font color="{color}"><small> {gszf}%</small></font></p>'
         sio_content3+='<p>再等等看吧</p>'
 
